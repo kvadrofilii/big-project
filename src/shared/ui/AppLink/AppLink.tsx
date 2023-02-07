35a70defeb80e7d59c5ad94cbca07daac0c4ac49
Michael Yakovlev <kvadrofilii@gmail.com>
2023-02-07 21:18:43 +0300

    add AppLink

---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,25 @@
+import { FC } from 'react';
+import { Link, LinkProps } from 'react-router-dom';
+import { cn } from 'shared/lib/classNames';
+import css from './AppLink.m.css';
+
+export type AppLinkTheme = 'primary' | 'secondary';
+
+export interface AppLinkProps extends LinkProps {
+  className?: string;
+  theme?: AppLinkTheme;
+}
+
+export const AppLink: FC<AppLinkProps> = ({
+  to,
+  className,
+  children,
+  theme = 'primary',
+  ...otherProps
+}) => {
+  return (
+    <Link className={cn([css.root, className, css[theme]])} to={to} {...otherProps}>
+      {children}
+    </Link>
+  );
+};
