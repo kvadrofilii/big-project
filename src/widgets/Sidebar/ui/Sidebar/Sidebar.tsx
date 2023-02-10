a579405865437e6282b316f457517a9ec3609478
Michael Yakovlev <kvadrofilii@gmail.com>
2023-02-10 08:00:24 +0300

    add Sidebar component

---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,25 @@
+import { FC, useState } from 'react';
+import { cn } from 'shared/lib';
+import css from './Sidebar.m.css';
+import ArrowSquare from 'widgets/assets/icons/arrow-square.svg';
+import { Button } from 'shared/ui';
+
+interface SidebarProps {
+  className?: string;
+}
+
+export const Sidebar: FC<SidebarProps> = ({ className }) => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const onToggle = () => {
+    setCollapsed((prev) => !prev);
+  };
+
+  return (
+    <div className={cn([css.root, className], { [css.collapsed]: collapsed })}>
+      <Button variant="clear" type="button" onClick={onToggle} className={css.btn}>
+        <ArrowSquare />
+      </Button>
+    </div>
+  );
+};
