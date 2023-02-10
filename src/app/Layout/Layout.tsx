df95b4a1f4af01b56e88cece9fb7ba32c0cc1d94
Michael Yakovlev <kvadrofilii@gmail.com>
2023-02-10 07:58:01 +0300

    ref: Layout

---
+++ src/app/Layout/Layout.tsx
@@ -0,0 +1,20 @@
+import { Outlet } from 'react-router-dom';
+import { Footer } from 'widgets/Footer';
+import { Header } from 'widgets/Header';
+import { Sidebar } from 'widgets/Sidebar';
+import css from './Layout.m.css';
+
+export const Layout = () => {
+  return (
+    <div className={css.root}>
+      <Header />
+      <div className={css.body}>
+        <Sidebar className={css.sidebar} />
+        <div className={css.main}>
+          <Outlet />
+        </div>
+        <Footer className={css.footer} />
+      </div>
+    </div>
+  );
+};
