5c08923e5af4dc2f73c107c7e3f95a0a94fe3f0e
Michael Yakovlev <kvadrofilii@gmail.com>
2023-02-11 22:43:15 +0300

    add LangSelect & translate navigation

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -3,23 +3,28 @@ import { AppLink } from 'shared/ui';
 import { ThemeSwitcher } from 'shared/ui';
 import { cn } from 'shared/lib';
 import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import { LangSelect } from 'shared/ui';
 
 export interface HeaderProps {
   className?: string;
 }
 
 export const Header: FC<HeaderProps> = ({ className }) => {
+  const { t } = useTranslation();
+
   return (
     <header className={cn([css.root, className])}>
       <div className={css.wrapper}>
-        <AppLink theme="secondary" to={'/'}>
-          Главная
+        <AppLink theme="inverted" to={'/'}>
+          {t('MainPage')}
         </AppLink>
-        <AppLink theme="secondary" to={'/about'}>
-          О сайте
+        <AppLink theme="inverted" to={'/about'}>
+          {t('AboutPage')}
         </AppLink>
       </div>
       <div className={css.wrapper}>
+        <LangSelect />
         <ThemeSwitcher />
       </div>
     </header>
