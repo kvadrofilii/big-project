f0301f3d48c652339b47ec1f0ed9008d7b3bbf1f
Michael Yakovlev <kvadrofilii@gmail.com>
2023-02-11 22:37:27 +0300

    add import config/i18n for index

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app/App';
 import ThemeProvider from './shared/contexts/theme-context/ThemeProvider';
+import 'shared/config/i18n/i18n';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
-
 root.render(
   <React.StrictMode>
     <ThemeProvider>
       <App />
     </ThemeProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
