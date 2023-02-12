7b4f14152ae3f7af46a637ea190bcdeb575e4b3d
Michael Yakovlev <kvadrofilii@gmail.com>
2023-02-12 21:23:38 +0300

    add test classNames

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,33 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+  test('with only first param', () => {
+    const expected = 'class';
+
+    expect(classNames(['class'])).toBe(expected);
+  });
+
+  test('with param more', () => {
+    const expected = 'class1 class2';
+
+    expect(classNames(['class1', 'class2'])).toBe(expected);
+  });
+
+  test('with mods', () => {
+    const expected = 'class1 class2 hovered scroll';
+
+    expect(classNames(['class1', 'class2'], { hovered: true, scroll: true })).toBe(expected);
+  });
+
+  test('with mods false', () => {
+    const expected = 'class1 class2 hovered';
+
+    expect(classNames(['class1', 'class2'], { hovered: true, scroll: false })).toBe(expected);
+  });
+
+  test('with mods undefined', () => {
+    const expected = 'class1 class2 hovered';
+
+    expect(classNames(['class1', 'class2'], { hovered: true, scroll: undefined })).toBe(expected);
+  });
+});
