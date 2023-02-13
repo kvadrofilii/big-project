ce3efc64464184b90901c194d29d2244015b1a7c
Michael Yakovlev <kvadrofilii@gmail.com>
2023-02-13 07:44:13 +0300

    add auto change attribute html  lang

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,20 @@
+import { useEffect } from 'react';
+
+import { useTranslation } from 'react-i18next';
 import { useTheme } from 'shared/contexts/theme-context/useTheme';
 import { cn } from 'shared/lib';
 
 import { AppRouter } from './providers/router';
+
 import 'app/styles/index.css';
 
 const App = () => {
   const { theme } = useTheme();
+  const { i18n } = useTranslation();
+
+  useEffect(() => {
+    document.documentElement.setAttribute('lang', i18n.language);
+  }, [i18n.language]);
 
   return (
     <div className={cn(['app', theme])}>
