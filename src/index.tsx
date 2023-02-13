bdaf85bf4fe81c813fbd7fafe93520ae65840188
Michael Yakovlev <kvadrofilii@gmail.com>
2023-02-13 08:57:06 +0300

    lint index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client';
 
 import App from './app/App';
 import ThemeProvider from './shared/contexts/theme-context/ThemeProvider';
+
 import 'shared/config/i18n/i18n';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
