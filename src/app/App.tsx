6cc5dab697539f899c8c93cf9e1ed4b67f93b752
Michael Yakovlev <kvadrofilii@gmail.com>
2023-02-13 08:56:49 +0300

    rename folder AppRouter

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from 'react-i18next';
 import { useTheme } from 'shared/contexts/theme-context/useTheme';
 import { cn } from 'shared/lib';
 
-import { AppRouter } from './providers/router';
+import { AppRouter } from './providers/AppRouter';
 
 import 'app/styles/index.css';
 
