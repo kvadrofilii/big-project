ae5e577184ec0b73dd516096a7f4fc76f38205c4
Michael Yakovlev <kvadrofilii@gmail.com>
2023-03-07 08:54:37 +0300

    add Counter slice

---
+++ src/entities/Counter/ui/Counter.test.tsx
@@ -0,0 +1,30 @@
+import { userEvent } from '@storybook/testing-library';
+import { screen } from '@testing-library/react';
+import { ComponentRender } from 'shared/lib';
+
+import { Counter } from './Counter';
+
+describe('Counter', () => {
+  test('test render', () => {
+    ComponentRender(<Counter />, {
+      initialState: { counter: { value: 10 } },
+    });
+    expect(screen.getByTestId('value-title')).toHaveTextContent('10');
+  });
+
+  test('increment', () => {
+    ComponentRender(<Counter />, {
+      initialState: { counter: { value: 10 } },
+    });
+    userEvent.click(screen.getByTestId('increment-btn'));
+    expect(screen.getByTestId('value-title')).toHaveTextContent('11');
+  });
+
+  test('decrement', () => {
+    ComponentRender(<Counter />, {
+      initialState: { counter: { value: 10 } },
+    });
+    userEvent.click(screen.getByTestId('decrement-btn'));
+    expect(screen.getByTestId('value-title')).toHaveTextContent('9');
+  });
+});
