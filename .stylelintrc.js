bc9f207d6ca1eac9aea3230000aebfcbb0fedc42
Michael Yakovlev <kvadrofilii@gmail.com>
2023-03-31 08:56:40 +0300

    update npm packages

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,5 @@
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-clean-order'],
-  plugins: ['stylelint-order'],
   rules: {
     // Отключаю псевдо-класс global, чтобы никто глобально не переопределял стили у компонентов
     'selector-pseudo-class-disallowed-list': ['global'],
