00f2ae3b48ea01f1523687b31d6bdb3257da2568
Michael Yakovlev <kvadrofilii@gmail.com>
2023-04-02 22:27:56 +0300

    fix imports

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -1,12 +1,13 @@
 import { useTranslation } from 'react-i18next';
-import { useDispatch, useSelector } from 'react-redux';
-import { Button } from 'shared/ui/Button/Button';
+import { useSelector } from 'react-redux';
+import { useAppDispatch } from 'shared/lib';
+import { Button } from 'shared/ui';
 
 import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
 import { counterActions } from '../model/slice/counter.slice';
 
 export const Counter = () => {
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
   const counterValue = useSelector(getCounterValue);
   const { t } = useTranslation();
 
