5348aed832af27c6ce74021e0d0483059b32b2f4
Michael Yakovlev <kvadrofilii@gmail.com>
2023-04-19 17:49:14 +0300

    del Counter

--- src/entities/Counter/index.ts
+++
@@ -1,6 +0,0 @@
-import { counterReducer } from './model/slice/counter.slice';
-import { Counter } from './ui/Counter';
-
-import type { CounterSchema } from './model/types/counterSchema';
-
-export { counterReducer, Counter, CounterSchema };
