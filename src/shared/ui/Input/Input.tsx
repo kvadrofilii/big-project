d21640a2598056bbac641ab760ae3c59ab7e6608
Michael Yakovlev <kvadrofilii@gmail.com>
2023-05-04 08:19:08 +0300

    feat: value & onChange for Input

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,13 +1,31 @@
-import { memo, InputHTMLAttributes } from 'react';
+import { memo, InputHTMLAttributes, ChangeEvent } from 'react';
 
 import clsx from 'clsx';
 
 import css from './Input.m.css';
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+type HTMLInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'>;
+
+interface InputProps extends HTMLInputProps {
   className?: string;
+  value?: string | number;
+  onChange?: (value: string) => void;
 }
 
-export const Input = memo(function Input({ className, ...rest }: InputProps) {
-  return <input data-testid="input" className={clsx(css.root, className)} {...rest} />;
+export const Input = memo(function Input(props: InputProps) {
+  const { className, value, onChange, ...rest } = props;
+
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange?.(e.target.value);
+  };
+
+  return (
+    <input
+      data-testid="input"
+      className={clsx(css.root, className)}
+      value={value}
+      onChange={onChangeHandler}
+      {...rest}
+    />
+  );
 });
