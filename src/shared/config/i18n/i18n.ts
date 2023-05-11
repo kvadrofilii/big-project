ec139e8753156a552eeff63ea24f441e35ac06cf
Michael Yakovlev <kvadrofilii@gmail.com>
2023-05-11 07:43:36 +0300

    fix config for backend i18n

--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -3,15 +3,21 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
+const ns = ['translation', 'main-page', 'about-page', 'profile'];
+
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    lng: 'ru',
     fallbackLng: 'ru',
     debug: __IS_DEV__,
     returnNull: false,
-
+    ns,
+    backend: {
+      loadPath: 'locales/{{lng}}/{{ns}}.json',
+    },
     interpolation: {
       escapeValue: false,
     },
