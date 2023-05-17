1f158ae4de69a45bb8060694b4f341086c15efd5
Michael Yakovlev <kvadrofilii@gmail.com>
2023-05-17 23:06:29 +0300

    fix webpack config for storybook

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -7,15 +7,25 @@ import { cssLoader } from '../webpack/loaders/cssLoader';
 import { svgLoader } from '../webpack/loaders/svgLoader';
 
 export default ({ config }: { config: webpack.Configuration }) => {
+  const src = path.resolve(__dirname, '..', '..', 'src');
+  config!.resolve!.modules = [src, 'node_modules'];
+
   config.resolve?.extensions?.push('.ts', '.tsx');
-  config!.resolve!.modules = [path.resolve(__dirname, '../../src'), 'node_modules'];
 
   const rules = config.module?.rules as RuleSetRule[];
-
-  // Отключаю встроенный обработчик svg
+  // Отключаю встроенные обработчики
   config!.module!.rules = rules.map((rule: RuleSetRule) => {
+    // Удаляю svg из asset-модуля
     if (/svg/.test(rule.test as string)) {
-      return { ...rule, exclude: /\.svg$/i };
+      return {
+        ...rule,
+        test: /\.(ico|jpg|jpeg|png|apng|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/,
+      };
+    }
+
+    // Отключаю встроенный обработчик стилей
+    if (/css/.test(rule.test as string)) {
+      return {};
     }
 
     return rule;
@@ -24,15 +34,6 @@ export default ({ config }: { config: webpack.Configuration }) => {
   // Подключаю свой обработчик для svg
   config.module?.rules?.push(svgLoader());
 
-  // Отключаю встроенный обработчик стилей
-  config!.module!.rules = rules.map((rule: RuleSetRule) => {
-    if (/(s[ac]ss|css)/.test(rule.test as string)) {
-      return { ...rule, exclude: /\.(s[ac]ss|css)$/i };
-    }
-
-    return rule;
-  });
-
   // Подключаю свой обработчик для стилей
   config.module?.rules?.push(cssLoader(true));
 
@@ -40,7 +41,8 @@ export default ({ config }: { config: webpack.Configuration }) => {
   config.plugins?.push(
     new DefinePlugin({
       __IS_DEV__: true,
-      __API__: '',
+      __API__: JSON.stringify(''),
+      __PROJECT__: JSON.stringify('storybook'),
     }),
   );
 
