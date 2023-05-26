c119c7348b4265e7a580b534b16eda68bfd708a0
Michael Yakovlev <kvadrofilii@gmail.com>
2023-05-26 07:19:12 +0300

    refactor: links for index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
+import App from 'app/App';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 import { StoreProvider } from 'app/providers/StoreProvider';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import ThemeProvider from 'shared/contexts/theme-context/ThemeProvider';
 import { PageError } from 'widgets/PageError';
-
-import App from './app/App';
-import ThemeProvider from './shared/contexts/theme-context/ThemeProvider';
 
 import 'shared/config/i18n/i18n';
 import 'app/styles/index.css';
