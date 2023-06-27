7dcb2d6ac20d4eae3e7fa04131dbdac455270ddd
Michael Yakovlev <kvadrofilii@gmail.com>
2023-06-27 23:12:55 +0300

    feat Page component

---
+++ src/shared/ui/Page/Page.story.tsx
@@ -0,0 +1,16 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Page } from './Page';
+
+export default {
+  title: '/Page',
+  component: Page,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Page>;
+
+const Template: ComponentStory<typeof Page> = (args) => <Page {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -11,6 +11,7 @@ export { LangSelect } from './LangSelect/LangSelect';
 export { Loader } from './Loader/Loader';
 export { Modal } from './Modal/Modal';
 export { Overlay } from './Overlay/Overlay';
+export { Page } from './Page/Page';
 export { Portal } from './Portal/Portal';
 export { Select } from './Select/Select';
 export { Skeleton } from './Skeleton/Skeleton';
