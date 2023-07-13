b1c626e7d726d36a3e32a8371545b3ffe44fe926
Michael Yakovlev <kvadrofilii@gmail.com>
2023-07-13 08:34:26 +0300

    feat: Tabs component

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,3 +1,5 @@
+import { type } from 'os';
+
 export { AppLink } from './AppLink/AppLink';
 export { Avatar } from './Avatar/Avatar';
 export { Button } from './Button/Button';
@@ -14,5 +16,9 @@ export { Overlay } from './Overlay/Overlay';
 export { Portal } from './Portal/Portal';
 export { Select } from './Select/Select';
 export { Skeleton } from './Skeleton/Skeleton';
+export { Tabs } from './Tabs/Tabs';
 export { Text } from './Text/Text';
 export { ThemeSwitcher } from './ThemeSwitcher/ThemeSwitcher';
+
+export type { TabItem } from './Tabs/Tabs.types';
+export type { SelectOption } from './Select/Select.types';
