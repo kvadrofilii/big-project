9b6ba417a2cfe442c8abc9a03d820a39985d7138
Michael Yakovlev <kvadrofilii@gmail.com>
2023-07-20 08:47:41 +0300

    fix: return config for api.interceptors

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -5,10 +5,11 @@ export const $api = axios.create({
   baseURL: __API__,
 });
 
-// @ts-ignore
 $api.interceptors.request.use((config) => {
   if (config.headers) {
     // eslint-disable-next-line no-param-reassign
     config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
   }
+
+  return config;
 });
