0eb3cac4d95f71ec4db4a192dbd7d85d2329779d
Michael Yakovlev <kvadrofilii@gmail.com>
2023-09-11 08:30:30 +0300

    feat: add asyncReducers in ComponentRender for tests

--- src/shared/lib/tests/ComponentRender/ComponentRender.tsx
+++ src/shared/lib/tests/ComponentRender/ComponentRender.tsx
@@ -5,18 +5,20 @@ import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
 import { I18nextProvider } from 'react-i18next';
 import { MemoryRouter } from 'react-router-dom';
 import i18nForTests from 'shared/config/i18n/i18n-for-test';
+import { ReducersMapObject } from '@reduxjs/toolkit';
 
 export interface ComponentRenderOptions {
   route?: string;
   initialState?: DeepPartial<StateSchema>;
+  asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>;
 }
 
 export function ComponentRender(component: ReactNode, options: ComponentRenderOptions = {}) {
-  const { route = '/', initialState } = options;
+  const { route = '/', initialState, asyncReducers } = options;
 
   return render(
     <MemoryRouter initialEntries={[route]}>
-      <StoreProvider initialState={initialState}>
+      <StoreProvider initialState={initialState} asyncReducers={asyncReducers}>
         <I18nextProvider i18n={i18nForTests}>{component}</I18nextProvider>
       </StoreProvider>
     </MemoryRouter>,
