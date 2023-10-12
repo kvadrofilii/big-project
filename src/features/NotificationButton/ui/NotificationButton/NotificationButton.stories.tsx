10fe3ee156beea4fe9853ae739450482988ab3f6
Michael Yakovlev <kvadrofilii@gmail.com>
2023-10-12 07:40:01 +0300

    fix: title for NotificationButton story

--- src/features/NotificationButton/ui/NotificationButton/NotificationButton.stories.tsx
+++ src/features/NotificationButton/ui/NotificationButton/NotificationButton.stories.tsx
@@ -3,7 +3,7 @@ import { Meta, StoryObj } from '@storybook/react';
 import { NotificationButton } from './NotificationButton';
 
 const meta = {
-  title: '/NotificationButton',
+  title: 'features/NotificationButton',
   component: NotificationButton,
   tags: ['autodocs'],
 } satisfies Meta<typeof NotificationButton>;
