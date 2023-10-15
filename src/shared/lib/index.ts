75b7bcb27adba3e5e6423d9a6bdd21e4a6ba0ddf
Michael Yakovlev <kvadrofilii@gmail.com>
2023-10-15 20:55:04 +0300

    feat: useDeviceDetect

--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -1,12 +1,16 @@
 export { useAppDispatch } from './hooks/useAppDispatch/useAppDispatch';
 export { useAppSelector } from './hooks/useAppSelector/useAppSelector';
+export { useDebounce } from './hooks/useDebounce/useDebounce';
+export { useDeviceDetect } from './hooks/useDeviceDetect/useDeviceDetect';
 // export { useDynamicModuleLoad } from './hooks/useDynamicModuleLoad/useDynamicModuleLoad';
 // export type { ReducersList } from './hooks/useDynamicModuleLoad/useDynamicModuleLoad';
-export type { ReducersList } from './components/DynamicModuleLoader/DynamicReducerLoader.types';
-export { DynamicReducerLoader } from './components/DynamicModuleLoader/DynamicReducerLoader';
-export { useInitialEffect } from './hooks/useInitialEffect/useInitialEffect';
 export { useHover } from './hooks/useHover/useHover';
+export { useInitialEffect } from './hooks/useInitialEffect/useInitialEffect';
 export { useInfiniteScroll } from './hooks/useInfiniteScroll/useInfiniteScroll';
 export { useThrottle } from './hooks/useThrottle/useThrottle';
-export { useDebounce } from './hooks/useDebounce/useDebounce';
+
+export { DynamicReducerLoader } from './components/DynamicModuleLoader/DynamicReducerLoader';
+
 export { addQueryParams } from './url/addQueryParams/addQueryParams';
+
+export type { ReducersList } from './components/DynamicModuleLoader/DynamicReducerLoader.types';
