5cb6dbdc9a20f72bfaad0515a48e83dd9a944e23
Michael Yakovlev <kvadrofilii@gmail.com>
2023-10-15 20:55:40 +0300

    feat: color inherit for Typography

--- src/shared/ui/Typography/types/typography.types.ts
+++ src/shared/ui/Typography/types/typography.types.ts
@@ -1,3 +1,12 @@
-export type TypographyColor = 'dark' | 'light' | 'primary' | 'secondary' | 'error' | 'warning' | 'info' | 'success';
+export type TypographyColor =
+  | 'dark'
+  | 'light'
+  | 'primary'
+  | 'secondary'
+  | 'error'
+  | 'warning'
+  | 'info'
+  | 'success'
+  | 'inherit';
 
 export type TypographyAlign = 'start' | 'end' | 'center' | 'justify';
