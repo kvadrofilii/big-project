51952df6f609de788c156de49984ec570ece1614
Michael Yakovlev <kvadrofilii@gmail.com>
2023-11-19 21:47:13 +0300

    feat: update ArticlesRecommendationsList.stories

--- src/entities/Article/testing.ts
+++ src/entities/Article/testing.ts
@@ -1,2 +1,4 @@
 export { articleDetailsReducer } from './model/slices/articleDetails.slice';
 export { article } from './mocks/data';
+
+export type { Article } from './model/types/article.types';
--- src/features/ArticlesRecommendationsList/ui/ArticlesRecommendationsList/ArticlesRecommendationsList.stories.tsx
+++ src/features/ArticlesRecommendationsList/ui/ArticlesRecommendationsList/ArticlesRecommendationsList.stories.tsx
@@ -2,7 +2,20 @@ import { StoreDecorator } from '@/shared/config/testing';
 
 import { ArticlesRecommendationsList } from './ArticlesRecommendationsList';
 
+import type { Article } from '@/entities/Article/testing';
 import type { Meta, StoryObj } from '@storybook/react';
+
+const article: Article = {
+  id: '1',
+  img: '',
+  createdAt: '',
+  views: 123,
+  user: { id: '1', username: '123' },
+  blocks: [],
+  type: [],
+  title: '123',
+  subtitle: 'asfsa',
+};
 
 const meta = {
   title: 'features/ArticlesRecommendationsList',
@@ -16,4 +29,18 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {},
   decorators: [StoreDecorator({})],
+  parameters: {
+    mockData: [
+      {
+        url: `${__API__}/articles?_limit=3`,
+        method: 'GET',
+        status: 200,
+        response: [
+          { ...article, id: '1' },
+          { ...article, id: '2' },
+          { ...article, id: '3' },
+        ],
+      },
+    ],
+  },
 };
